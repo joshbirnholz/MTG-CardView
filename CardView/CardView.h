6c1a6f5ee3acb074e0244bfccd97b97019274dc5
Josh Birnholz <josh@birnholz.com>
2020-05-11 15:15:55 -0400

    Initial Commit

---
+++ CardView/CardView.h
@@ -0,0 +1,19 @@
+//
+//  CardView.h
+//  CardView
+//
+//  Created by Josh Birnholz on 5/11/20.
+//  Copyright © 2020 Josh Birnholz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardView.
+FOUNDATION_EXPORT double CardViewVersionNumber;
+
+//! Project version string for CardView.
+FOUNDATION_EXPORT const unsigned char CardViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardView/PublicHeader.h>
+
+
